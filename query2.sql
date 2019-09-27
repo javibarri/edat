1a844efd905547db23c388f86f741fff81843a8a
javibarri <50178579+javibarri@users.noreply.github.com>
2019-09-27 20:27:29 +0200

    Add files via upload

---
+++ query2.sql
@@ -0,0 +1,3 @@
+﻿select city_id, city
+	from city 
+	where  address.address_id = customer.address_id and customer.customer_id = rental.customer_id and rental.inventory_id = inventory.inventory_id and inventory.film_id = film_actor.film_id and film_actor.actor_id = actor.actor_id and actor.first_name = 'Bob' and actor.last_name = 'Fawcett' and city.city_id = address.city_id;
